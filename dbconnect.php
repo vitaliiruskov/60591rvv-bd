9bee686d7b1fa5ba16324f1fb969ae5f399e48ab
Vitalii <Vitalii@BOOK-ULHHUG0285>
2022-11-22 12:08:44 +0500

    Изненение типа подключения

--- dbconnect.php
+++ dbconnect.php
@@ -1,13 +1,17 @@
 <?php
 
 require __DIR__ . '/vendor/autoload.php'; //загрузка всех установленных библиотек
-use dotenv\Dotenv;                        //импорт класса Dotenv из пространства имен dotenv
+use Dotenv\Dotenv;                        //импорт класса Dotenv из пространства имен dotenv
 if (file_exists(__DIR__."/.env"))
 {
      $dotenv = Dotenv::createImmutable(__DIR__);
      $dotenv->load(); //все параметры окружения помещаются в массив $_ENV
 }
 
-$conn = new mysqli($_ENV['dbhost'], $_ENV['dbuser'], $_ENV['dbpassword'], $_ENV['dbname']);
-if($conn->connect_errno) echo "Ошибка";
-else echo "БД Подключена";
+try {
+    $conn = new PDO("mysql:host=$_ENV[dbhost];dbname=$_ENV[dbname]", $_ENV['dbuser'], $_ENV['dbpassword']);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "ERROR" . $e->getMessage(), $e->getCode();
+    die();
+}
