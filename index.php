293170161a2eae055b593385eb4f563398afd9ac
Vitalii <Vitalii@BOOK-ULHHUG0285>
2023-02-20 21:52:34 +0500

    Реализация архитектуры MVC - маршрутизация

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php
+
+use Framework\Request;
+use Framework\Router;
+use Framework\Application;
+
+if ( file_exists(dirname(__FILE__).'/vendor/autoload.php') ) {
+    require_once dirname(__FILE__).'/vendor/autoload.php';
+}
+
+$request = new Request();
+Application::init();
+echo (new Router($request))->getContent();
+
+exit();
+
 require('dbconnect.php');
+
 require ('auth.php');
 require('components/header.php');
 require ('components/message.php');
